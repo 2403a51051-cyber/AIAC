358d13db6229c763d96b9c36099f7d319e3337e1
2403a51051-cyber <2403a51051@sru.edu.in>
2025-09-24 21:51:56 +0530

    Add files via upload

---
+++ lab13.4/Task2.py
@@ -0,0 +1,5 @@
+words = ["AI", "helps", "in", "refactoring", "code"]
+sentence = " ".join(words)
+print(sentence)
+
+
---
+++ lab13.4/Task3.py
@@ -0,0 +1,4 @@
+student_scores = {"Alice": 85, "Bob": 90}
+print(student_scores.get("Charlie", "Not Found"))
+
+
